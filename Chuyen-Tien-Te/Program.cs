6c76444a118dc56e4f2335415f9978336d7f90db
Thao Trinh <thaottb99@gmail.com>
2023-08-17 21:35:22 +0700

    [Bài tập] Ứng dụng chuyển đổi tiền tệ
    Quiz 4

---
+++ Chuyen-Tien-Te/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int VND = 230000;
+        Console.Write("Mời bạn nhập số tiền cần quy đổi: ");
+        string tien= Console.ReadLine();
+        int tienQuyDoi = int.Parse(tien);
+        int USD = tienQuyDoi * VND;
+        Console.WriteLine("{0} USD bằng {1} VND",tien,USD);
+
+
+    }
+}
