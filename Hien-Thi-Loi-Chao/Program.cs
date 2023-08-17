7c8eec857301c95226908ce4e7aa3140067fa072
Thao Trinh <thaottb99@gmail.com>
2023-08-17 21:21:23 +0700

    [Bài tập] Hiển thị lời chào
    Quiz 3

---
+++ Hien-Thi-Loi-Chao/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello "+name);
+    }
+}
