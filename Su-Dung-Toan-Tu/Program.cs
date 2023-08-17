60d8aaafcd4078991592a6a6116db31545ca6bf8
Thao Trinh <thaottb99@gmail.com>
2023-08-17 21:14:10 +0700

    [Thực hành] Sử dụng toán tử
    Quiz 2

---
+++ Su-Dung-Toan-Tu/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        float w;
+        float h;
+
+        Console.Write("Enter width: ");
+        w= float.Parse(Console.ReadLine());
+        Console.Write("Enter height: ");
+        h= float.Parse(Console.ReadLine());
+
+        float area = w + h;
+        Console.WriteLine("Area is: "+ area);
+    }
+}
