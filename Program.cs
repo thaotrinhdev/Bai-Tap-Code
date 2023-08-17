53a8d9297a60d9b7d229d0c547855d0bbbaa6bd7
Thao Trinh <thaottb99@gmail.com>
2023-08-17 20:06:55 +0700

    Bài Tập Hiển thị thời gian của hệ thống
    Quiz 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime localTime = DateTime.Now;
+        System.Console.WriteLine(" Thời gian hiện tại là: " + localTime );
+    }
+    
+}
