628d83f54ab20f96ecbae6c77c349223b167d232
Thao Trinh <thaottb99@gmail.com>
2023-08-24 22:14:03 +0700

    [Thực hành] Kiểm tra số nguyên tố
    Quiz 2

---
+++ Kiem-Tra-So-Nguyen-To/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Number: ");
+        int a = int.Parse(Console.ReadLine());
+        bool isPrime = true;
+        for(int i = 2; i<a; i++)
+        {
+            if(a%1==0)
+            isPrime=false;
+        }
+        Console.Write(isPrime);
+    }
+}
