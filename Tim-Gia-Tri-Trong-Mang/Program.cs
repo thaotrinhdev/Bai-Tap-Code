14f31e9e573902ec74603ff57cf7c3405ea6b14c
Thao Trinh <thaottb99@gmail.com>
2023-08-25 19:29:25 +0700

    [Thực hành] Tìm giá trị trong mảng
    Quiz 2

---
+++ Tim-Gia-Tri-Trong-Mang/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[]student = {"Linh", "Nguyet","Thi","Ngoc","Tran"};
+        Console.WriteLine("Enter a name's studen: ");
+        string name = Console.ReadLine();
+
+        for(int i = 0 ; i<student.Length;i++)
+        {
+            if(name.ToUpper() == student[i].ToUpper())
+            {Console.WriteLine("Position of the students in the list" + name + "is:"+(i+1));
+            return;}
+        }
+         Console.WriteLine("Not found" + name + "in the list.");
+    }
+}
