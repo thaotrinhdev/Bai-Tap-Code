dbac4b0ca1d3572b90e22f92f5fcdfa44c7f4c60
Thao Trinh <thaottb99@gmail.com>
2023-08-25 19:11:06 +0700

    [Thực hành] Tính tổng các phần tử của mảng
    Quiz 1

---
+++ Tính-Tong-Phan-Tu-Mang/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int [] num = new int []{2,5,9,7,6};
+
+        Console.WriteLine(num[1]);
+        Console.WriteLine(num[2]);
+        Console.WriteLine(num[4]);
+
+        int total = 0;
+        for(int i=0; i<num.Length;i++)
+        {
+            total= total+num[i];
+        }
+        Console.WriteLine($"Total is: {total}");
+    }
+    
+}
