d189759a084b6fe0d94fcd5517d914879a963e2e
Thao Trinh <thaottb99@gmail.com>
2023-08-26 22:32:52 +0700

    [Thực hành] Tìm giá trị lớn nhất trong mảng
    Quiz 2

---
+++ Tim-gia-tri-lon-nhat/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int size;
+        int [] array;
+        do
+        {
+            Console.WriteLine("nhap so luong so ban muon co trong mang: ");
+            size=int.Parse(Console.ReadLine());
+            if(size>20)
+            {
+                Console.WriteLine("Khong dur dieu kien");
+            }
+        }
+        while(size>20);
+        array=new int[size];
+        for(int i=0;i<array.Length;i++)
+        {
+            Console.WriteLine($"ban da nhap {i+1} vao mang");
+            array[i]= int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Mang cúa ban co nhung so sau: ");
+        for(int j=0;j<array.Length;j++)
+        {
+            Console.WriteLine(array[j]+"\t");
+        }
+        int max= array[0];
+        int index=1;
+        for(int j=0;j<array.Length;j++)
+        {
+            if(array[j]>max)
+            {
+                max=array[j];
+                index =j+1;
+            }
+        }
+        Console.WriteLine("The largest property value in the list is " + max + " ,at position " + index);
+    }
+}
