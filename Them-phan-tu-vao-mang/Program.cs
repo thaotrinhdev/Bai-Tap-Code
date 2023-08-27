c13c7082e2ca7af911eb9396009d6a5bb9abb130
Thao Trinh <thaottb99@gmail.com>
2023-08-27 14:47:46 +0700

    [Bài tập] Thêm phần tử vào mảng
    Quiz 3

---
+++ Them-phan-tu-vao-mang/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int [] array = new int[5];
+            int c;
+            
+            Console.Write("Moi ban nhap so can chen: ");
+            int a= int.Parse(Console.ReadLine());
+            Console.Write("Moi ban nhap vị trí can chen: ");
+            int b= int.Parse(Console.ReadLine());
+            
+            Console.Write("In mang ban dau: ");
+            for(int i =0;i<b;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for(int i = a; i>=b; i--)
+            {
+                array[i]= array[i-1];
+            }
+            array[b]=a;
+            Console.WriteLine("Sau khi chen ta co mang như sau: ");
+            for(int i=0; i<=array.Length;i++)
+            {
+                Console.WriteLine(array[i]+"\t");
+            }
+
+            
+        }
+    }
+}
