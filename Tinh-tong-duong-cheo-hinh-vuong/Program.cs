de6390348b719f9418bf08fdb289c7ef0baac770
Thao Trinh <thaottb99@gmail.com>
2023-08-30 19:11:31 +0700

    [Bài tập] Mảng hai chiều – tính tổng các số ở đường chéo chính
    Quiz 5

---
+++ Tinh-tong-duong-cheo-hinh-vuong/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array= {{1,4,5},{4,7,3},{2,6,9}};
+            int total= 0;
+            for(int i = 0; i<array.GetLength(0);i++)
+            {
+                total+= array[i,i];
+            }
+            Console.WriteLine($"Total is {total}");
+
+        }
+    }
+}
