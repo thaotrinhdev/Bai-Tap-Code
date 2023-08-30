5670ccdbe45e7b693b331d9cc625922623312ef1
Thao Trinh <thaottb99@gmail.com>
2023-08-30 19:22:13 +0700

    [Thực hành] Chương trình chuyển đổi nhiệt độ
    Quiz 1

---
+++ Chuyen-doi-nhiet-do/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nhietDoC= 50;
+            double nhietDoF= doC(nhietDoC);
+            Console.WriteLine($"Tong: {nhietDoF}");
+        }
+        public static double doC(double c)
+        {
+            double f=(9.0/5)*c+32;
+            return f;
+        }
+        public static double doF(double f)
+        {
+            double c =(5.0 / 9) * (f - 32);
+            return c;
+        }
+    }
+}
