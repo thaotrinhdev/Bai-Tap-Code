b8af8be42505da43e1ad7814e5e6db011c21545c
Thao Trinh <thaottb99@gmail.com>
2023-09-03 19:57:51 +0700

    Thực hành] Mảng hai chiều – tạo bản đồ trò chơi MineSweeper
    Quiz 5

---
+++ Tro-choi-MineSweeper/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] map ={
+                {"*","","","",""},
+                {"","","","*",""},
+                {"","","*","","*"},
+                {"","","","",""},
+            };
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                for (int y = 0; y < map.GetLength(0); y++)
+                {
+                    if (map[y, x] == "*")
+                    {
+
+                    }
+                    else
+                    {
+                        int dem_so_bom = 0;
+
+                        int[,] xung_quanh = {
+                            {y-1,x-1},{y-1,x+0},{y-1,x+1},
+                            {y+0,x-1}          ,{y+0,x+1},
+                            {y+1,x-1},{y+1,x+0},{y+1,x+1}
+                        };
+                        for (int i = 0; i < xung_quanh.GetLength(0); i++)
+                        {
+                            if (xung_quanh[i, 0] < 0 || xung_quanh[i, 1] < 0
+                            || xung_quanh[i, 0] >= map.GetLength(0) || xung_quanh[i, 1] >= map.GetLength(1))
+                            {
+
+                            }
+                            else
+                            {
+                                if (map[xung_quanh[i, 0], xung_quanh[i, 1]] == "*")
+                                {
+                                    dem_so_bom++;
+                                }
+                            }
+                        }
+                        map[y, x] = dem_so_bom.ToString();
+                    }
+                }
+            }
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    Console.Write(map[y, x]);
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
