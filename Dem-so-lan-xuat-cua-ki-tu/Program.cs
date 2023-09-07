272e35a456289be39c351f0e8b603729f8c9ff9d
Thao Trinh <thaottb99@gmail.com>
2023-09-07 22:11:15 +0700

    [Bài tập] Viết phương thức đếm số lần xuất hiện của ký tự trong chuỗi
    Quiz 3

---
+++ Dem-so-lan-xuat-cua-ki-tu/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int [] Name = {1,4,6,7,88,9,0,22,7,5,7,22,5,7,8,99};
+            Console.WriteLine(",4,6,7,88,9,0,22,7,5,7,22,5,7,8,99");
+            Console.Write("Enter the character you want to delete: ");
+            int a =int.Parse(Console.ReadLine());
+            for(int i =0 ; i<Name.Length;i++)
+            {
+                if(Name[i]== a)
+                {
+                    count++;
+                }
+                
+            }
+            Console.Write($"{a} has {count} times");
+            
+        }
+    }
+}
