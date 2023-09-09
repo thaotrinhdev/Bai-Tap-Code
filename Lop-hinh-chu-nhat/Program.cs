b674cebd4738a631de5c88a2d4da3ba6c706cb52
Thao Trinh <thaottb99@gmail.com>
2023-09-09 13:43:09 +0700

    [Thực hành] Lớp hình chữ nhật
    Quiz 1

---
+++ Lop-hinh-chu-nhat/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moi ban nhap chieu dai: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Moi ban nhap chieu cao: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Rectangle retangle = new Rectangle(a,b);
+            Console.WriteLine("Hinh chu nhat \n"+ retangle.Display());
+            Console.WriteLine("Chu vi: "+ retangle.GetPerimeter()+"cm");
+            Console.WriteLine("Dien tich: "+ retangle.GetArea()+"cm");
+
+
+
+        }
+    }
+}
