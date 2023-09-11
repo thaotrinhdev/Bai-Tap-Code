f7ebffabe8ab913bf77c72a3044d2eb21457dcce
Thao Trinh <thaottb99@gmail.com>
2023-09-11 19:17:34 +0700

    [Thực hành] Static Method
    Quiz3

---
+++ Static-Method/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Change();
+            Student s1 = new Student(33,"Khanh");
+            Student s2 = new Student(344,"Chi");
+
+            s1.Display();
+            s2.Display();
+        }
+    }
+}
