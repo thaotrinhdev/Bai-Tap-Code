558a3cff7bfd1d736ca318a9e93eab719c1f26f9
Thao Trinh <thaottb99@gmail.com>
2023-09-12 22:34:44 +0700

    [Thực hành] Hệ các đối tượng hình học
    Quiz 1

---
+++ He-cac-doi-tuong-hinh-hoc/Shape.cs
@@ -0,0 +1,35 @@
+public class Shape
+{
+    private string color = "green";
+    private bool filled = true;
+    public Shape(){}
+    public Shape(string color, bool filled)
+    {
+        this.color=color;
+        this.filled=filled;
+    }
+    public string GetColor()
+    {
+        return color;
+    }
+    public void SetColor(string color)
+    {
+        this.color= color;
+    }
+    public bool IsFilled()
+    {
+        return filled;
+    }
+    public void SetFilled(bool filled)
+    {
+        this.filled=filled;
+    }
+    public override string ToString()
+    {
+        return "A Shape with color of "
+            + GetColor()
+            + " and "
+            + (IsFilled() ? "filled" : "not filled");
+    }
+
+}
