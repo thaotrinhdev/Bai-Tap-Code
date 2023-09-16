6ce23353c11997623a0c1f2db4762921e847c249
Thao Trinh <thaottb99@gmail.com>
2023-09-16 17:22:35 +0700

    [Thực hành] Sử dụng lớp IndexOutOfRangeException
    Quiz 1

---
+++ IndexOutOfRangeException/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> character = new List<char>();
+                // InsertRange: Chen doi tuong
+                character.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = character[6];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
