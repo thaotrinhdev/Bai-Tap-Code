5e7695258da9058cfbc27451442eff807600f041
Thao Trinh <thaottb99@gmail.com>
2023-09-18 20:12:17 +0700

    [Bài tập] Đọc file .csv
    quiz 4

---
+++ Doc-CSV/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace Name
+{
+    class ReadTextFileExample
+    {
+        static void Main(string[] args)
+        {
+            string path = "file.csv";
+            StreamReader reader = new StreamReader(path);
+            StreamWriter writer = new StreamWriter("file_01.csv");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] spearator = line.Split(',');
+                spearator[5] = spearator[5].Replace("\"", "");
+                writer.Write(spearator[5]);
+                writer.WriteLine("");
+            }
+            reader.Close();
+            writer.Close();
+        }
+
+    }
+
+
+}
